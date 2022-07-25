cc524d432adb07b3366951cf47ad21e46ffc9ce8
rajanptr <99859538+rajanptr@users.noreply.github.com>
2022-07-25 21:22:33 +0530

    Add files via upload

---
+++ files/TV Shows/Stranger Things/Stranger Things.js
@@ -0,0 +1,51 @@
+function change1() {
+    document.getElementById('vplay').src = "https://kldvvpjjuyebtwzwvumi.supabase.co/storage/v1/object/sign/strangerthings/Stranger%20Things%20ep%201.mp4?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJzdHJhbmdlcnRoaW5ncy9TdHJhbmdlciBUaGluZ3MgZXAgMS5tcDQiLCJpYXQiOjE2NTgzOTYwNzEsImV4cCI6MTk3Mzc1NjA3MX0.18dhQ5AJ77UmOvKD0m36YqKkYwHAXFYVwEnpXEhx_iw&t=2022-07-21T09%3A34%3A30.067Z"
+    document.getElementById('headtitle').innerHTML = "Episode 1"
+    document.getElementById('next').innerHTML = "Next Episode"
+    document.getElementById('nextep').innerHTML = "Episode 2"
+}
+function change2() {
+    document.getElementById('vplay').src = "https://kldvvpjjuyebtwzwvumi.supabase.co/storage/v1/object/sign/strangerthings/Stranger%20Things%20_%20ep2.mp4?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJzdHJhbmdlcnRoaW5ncy9TdHJhbmdlciBUaGluZ3MgXyBlcDIubXA0IiwiaWF0IjoxNjU4Mzk4NDU3LCJleHAiOjE5NzM3NTg0NTd9.TiJ3E9nWPMaUciVn4ywa9h4yxfK2yCZ-dPl7-jPKE_U&t=2022-07-21T10%3A14%3A15.537Z"
+    document.getElementById('headtitle').innerHTML = "Episode 2"
+    document.getElementById('next').innerHTML = "Next Episode"
+    document.getElementById('nextep').innerHTML = "Episode 3"
+}
+function change3() {
+    document.getElementById('vplay').src = "https://kldvvpjjuyebtwzwvumi.supabase.co/storage/v1/object/sign/strangerthings/Stranger%20Things%20_%20ep3.mp4?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJzdHJhbmdlcnRoaW5ncy9TdHJhbmdlciBUaGluZ3MgXyBlcDMubXA0IiwiaWF0IjoxNjU4Mzk4NDMwLCJleHAiOjE5NzM3NTg0MzB9._5LS4EvHvNC_Rapgbz5CcrUWN0SmGHfg_ASKe2YHOaE&t=2022-07-21T10%3A13%3A49.182Z"
+    document.getElementById('headtitle').innerHTML = "Episode 3"
+    document.getElementById('next').innerHTML = "Next Episode"
+    document.getElementById('nextep').innerHTML = "Episode 4"
+}
+function change4() {
+    document.getElementById('vplay').src = "https://kldvvpjjuyebtwzwvumi.supabase.co/storage/v1/object/sign/strangerthings/Stranger%20Things%20_%20ep4.mp4?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJzdHJhbmdlcnRoaW5ncy9TdHJhbmdlciBUaGluZ3MgXyBlcDQubXA0IiwiaWF0IjoxNjU4Mzk4MzI2LCJleHAiOjE5NzM3NTgzMjZ9.XgSKfQtgpuB3YW1-ICaABgVOSusGLFWTgPF9nIDWM3s&t=2022-07-21T10%3A12%3A05.048Z"
+    document.getElementById('headtitle').innerHTML = "Episode 4"
+    document.getElementById('next').innerHTML = "Next Episode"
+    document.getElementById('nextep').innerHTML = "Watch Next Season"
+}
+
+// ===========
+
+function changes1() {
+    document.getElementById('vplay').src = "https://kldvvpjjuyebtwzwvumi.supabase.co/storage/v1/object/sign/strangerthings/Stranger%20things%20s2%20ep1.mp4?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJzdHJhbmdlcnRoaW5ncy9TdHJhbmdlciB0aGluZ3MgczIgZXAxLm1wNCIsImlhdCI6MTY1ODQxNDQzNiwiZXhwIjoxOTczNzc0NDM2fQ.kw32Gv4TxYPrm2pGxRGXdRmnUCuCXeVyX-IBXTphP30&t=2022-07-21T14%3A40%3A34.343Z"
+    document.getElementById('headtitle').innerHTML = "Season 2- Episode 1"
+    document.getElementById('next').innerHTML = "Next Episode"
+    document.getElementById('nextep').innerHTML = "Season 2- Episode 2"
+}
+function changes2() {
+    document.getElementById('vplay').src = "https://kldvvpjjuyebtwzwvumi.supabase.co/storage/v1/object/sign/strangerthings/Stranger things s2 ep2.mp4?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJzdHJhbmdlcnRoaW5ncy9TdHJhbmdlciB0aGluZ3MgczIgZXAyLm1wNCIsImlhdCI6MTY1ODQxNDQ0NywiZXhwIjoxOTczNzc0NDQ3fQ.ytmU2A_6oGy9bzTXYgFtvTjbasMs1bZjmfYDW93w6dA"
+    document.getElementById('headtitle').innerHTML = "Season 2- Episode 2"
+    document.getElementById('next').innerHTML = "Next Episode"
+    document.getElementById('nextep').innerHTML = "Season 2- Episode 3"
+}
+function changes3() {
+    document.getElementById('vplay').src = "https://kldvvpjjuyebtwzwvumi.supabase.co/storage/v1/object/sign/strangerthings/Stranger%20things%20s2%20ep3.mp4?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJzdHJhbmdlcnRoaW5ncy9TdHJhbmdlciB0aGluZ3MgczIgZXAzLm1wNCIsImlhdCI6MTY1ODQxNDQ1OSwiZXhwIjoxOTczNzc0NDU5fQ.ZE91Q55C1h2X2Y0TUvVoL_6yYTdanutnix6016Tq0PI&t=2022-07-21T14%3A40%3A57.678Z"
+    document.getElementById('headtitle').innerHTML = "Season 2- Episode 3"
+    document.getElementById('next').innerHTML = "Next Episode"
+    document.getElementById('nextep').innerHTML = "Season 2- Episode 4"
+}
+function changes4() {
+    document.getElementById('vplay').src = "https://kldvvpjjuyebtwzwvumi.supabase.co/storage/v1/object/sign/strangerthings/Stranger%20things%20s2%20ep4.mp4?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJzdHJhbmdlcnRoaW5ncy9TdHJhbmdlciB0aGluZ3MgczIgZXA0Lm1wNCIsImlhdCI6MTY1ODQxNDQ3MSwiZXhwIjoxOTczNzc0NDcxfQ.zzDT_7eYXIoB-U-chwHXaEvccY1_8fV-TQtCG8S-hMc&t=2022-07-21T14%3A41%3A09.379Z"
+    document.getElementById('headtitle').innerHTML = "Season 2- Episode 4"
+    document.getElementById('next').innerHTML = "Next Episode"
+    document.getElementById('nextep').innerHTML = "Thanks for Watching"
+}
