701f1ae1a218a99193492488ce7ce63934cd3973
rajanptr <99859538+rajanptr@users.noreply.github.com>
2022-09-24 08:59:26 +0530

    Update movie.js

--- main/movie.js
+++ main/movie.js
@@ -6,24 +6,24 @@
 function knowmore1(){
    document.getElementById('about-title').innerHTML = 'Avatar: The Way of Water'
    document.getElementById('about-topic').innerHTML = "Jake Sully and Ney'tiri have formed a family and are doing everything to stay together. However, they must leave their home and explore the regions of Pandora. When an ancient threat resurfaces, Jake must fight a difficult war against the humans."
-   document.getElementById('about-rele').innerHTML = 'Director :' + ' 16 December 2022 '
-   document.getElementById('about-dir').innerHTML =  'Release date :' + ' James Cameron'
+   document.getElementById('about-rele').innerHTML = 'Director :' + ' James Cameron'
+   document.getElementById('about-dir').innerHTML =  'Release date :' + ' 16 December 2022 '
    document.getElementById('about-comp').innerHTML =  'Distributed by :' + ' 20th Century Studios'
 }
 
 function knowmore2(){
    document.getElementById('about-title').innerHTML = 'Black Adam'
    document.getElementById('about-topic').innerHTML = "Nearly 5,000 years after he was bestowed with the almighty powers of the Egyptian gods -- and imprisoned just as quickly -- Black Adam is freed from his earthly tomb, ready to unleash his unique form of justice on the modern world."
-   document.getElementById('about-rele').innerHTML = 'Director :' + '  21 October 2022  '
-   document.getElementById('about-dir').innerHTML =  'Release date :' + ' Jaume Collet-Serra'
+   document.getElementById('about-rele').innerHTML = 'Director :' + ' Jaume Collet-Serra'
+   document.getElementById('about-dir').innerHTML =  'Release date :' + ' 21 October 2022  '
    document.getElementById('about-comp').innerHTML =  'Distributed by :' + ' Warner Bros. Pictures'
 }
 
 function knowmore3(){
    document.getElementById('about-title').innerHTML = 'One Piece Film: Red'
    document.getElementById('about-topic').innerHTML = "One Piece Film: Red is an upcoming Japanese anime fantasy action-adventure film directed by Gorō Taniguchi and produced by Toei Animation. It is the fifteenth feature film of the One Piece film series, based on the manga of the same name written and illustrated by Eiichiro Oda."
-   document.getElementById('about-rele').innerHTML = 'Director :' + '  6 August 2022 (Japan) '
-   document.getElementById('about-dir').innerHTML =  'Release date :' + ' Gorō Taniguchi'
+   document.getElementById('about-rele').innerHTML = 'Director :' + ' Gorō Taniguchi'
+   document.getElementById('about-dir').innerHTML =  'Release date :' + ' 6 August 2022 (Japan) '
    document.getElementById('about-comp').innerHTML =  'Distributed by :' + '  Toei Company'
 }
 
@@ -38,21 +38,27 @@ function knowmore4(){
 function knowmore5(){
    document.getElementById('about-title').innerHTML = 'Spider-Man: Across the Spider-Verse'
    document.getElementById('about-topic').innerHTML = "Miles Morales embarks on an epic adventure that will transport Brooklyn's full-time, friendly neighbourhood Spider-Man across the Multiverse to join forces with Gwen Stacy and a new team."
-   document.getElementById('about-rele').innerHTML = 'Director :' + ' 2 June 2023  '
-   document.getElementById('about-dir').innerHTML =  'Release date :' + ' Joaquim Dos Santos, Kemp Powers, Justin Thompson'
+   document.getElementById('about-rele').innerHTML = 'Director :' + ' Joaquim Dos Santos, Kemp Powers, Justin Thompson'
+   document.getElementById('about-dir').innerHTML =  'Release date :' + ' 2 June 2023  '
    document.getElementById('about-comp').innerHTML =  'Distributed by :' + ' Sony Pictures'
 }
 
 function knowmore6(){
    document.getElementById('about-title').innerHTML = 'Fast X'
    document.getElementById('about-topic').innerHTML = "Fast X is an upcoming American action film directed by Louis Leterrier and written by Justin Lin and Dan Mazeau. It is the sequel to F9, serving as the tenth main installment, and the eleventh feature-film in the Fast & Furious franchise."
-   document.getElementById('about-rele').innerHTML = 'Director :' + ' 19 May 2023 '
-   document.getElementById('about-dir').innerHTML =  'Release date :' + '  Louis Leterrier'
+   document.getElementById('about-rele').innerHTML = 'Director :' +  '  Louis Leterrier'
+   document.getElementById('about-dir').innerHTML =  'Release date :' + ' 19 May 2023 '
    document.getElementById('about-comp').innerHTML =  'Distributed by :' + '  Universal Pictures'
 }
 
 // ==========================================================
 
 function adult(){
-   alert("This video contains content that some may find disturbing. Viewer discretion is advised.")
+     confirm ("This video contains content that some may find disturbing. Viewer discretion is advised.");   
+            if(con == true) {   
+                document.write ("User Want to continue");   
+            }    
+            else {   
+                document.write ("User does not want to continue");   
+            } 
 }
